3a28f94566492af27cbd0afe588a617ab97e162e
Sumit Binnani <sumit.binnani@gmail.com>
2017-06-25 21:15:06 +0530

    Implemented PID Controller

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+  p_error = 0;
+  d_error = 0;
+  i_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+  i_error = cte + i_error;
+  d_error = cte - p_error;
+  p_error = cte;
 }
 
 double PID::TotalError() {
+  return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
 
